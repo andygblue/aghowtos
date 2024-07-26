13baf568521b0764b7f97a69e21283bea8fe7e18
andygblue <andyg@uk.ibm.com>
2024-07-26 15:51:21 +0100

    Correct listed versions in comments - add newlines and separators to make output clearer

--- FixPacks/catalog-sources-16.1.0.1-ACE12.0.1-sc-2.sh
+++ FixPacks/catalog-sources-16.1.0.1-ACE12.0.1-sc-2.sh
@@ -1,6 +1,6 @@
 # Download and apply catalog sources
-# Updated for CP4I 2023.4.1 April Fixpack
-# Reference for versions in CP4I Docs here: https://www.ibm.com/docs/en/cloud-paks/cp-integration/2023.4?topic=images-adding-catalog-sources-cluster
+# Updated for CP4I 16.1.0.1 July Fixpack and ACE 12.0.1 Fixpack for SC-2
+# Reference for versions in CP4I Docs here: https://www.ibm.com/docs/en/cloud-paks/cp-integration/16.1.0?topic=images-adding-catalog-sources-cluster
 # Note that you will need to be logged into your cluster through `oc login login <openshift_url> -u <username> -p <password> -n <namespace>` and you will also need to have the ibm-pak in your %PATH%.
 
 #Set architecture to AMD64 - change this for Z
@@ -19,54 +19,54 @@ oc apply -f ~/.ibm-pak/data/mirror/${OPERATOR_PACKAGE_NAME}/${OPERATOR_VERSION}/
 oc apply -f ~/.ibm-pak/data/mirror/${OPERATOR_PACKAGE_NAME}/${OPERATOR_VERSION}/catalog-sources-linux-${ARCH}.yaml
 }
 
-# IBM CP4I Operator
+# IBM CP4I Operator 16.1.0.1 fixpack
 export OPERATOR_PACKAGE_NAME=ibm-integration-platform-navigator
-export OPERATOR_VERSION=7.2.4
+export OPERATOR_VERSION=7.3.1
 deploy_catalog_source
 
 # IBM Automation Assets
 export OPERATOR_PACKAGE_NAME=ibm-integration-asset-repository
-export OPERATOR_VERSION=1.6.4
+export OPERATOR_VERSION=1.7.0
 deploy_catalog_source
 
 # IBM API Connect
 export OPERATOR_PACKAGE_NAME=ibm-apiconnect
-export OPERATOR_VERSION=5.1.0
+export OPERATOR_VERSION=5.2.0
 deploy_catalog_source
 
-# IBM App Connect
+# IBM App Connect 12.0.1 SC-2 Fixpack
 export OPERATOR_PACKAGE_NAME=ibm-appconnect
-export OPERATOR_VERSION=11.5.0
+export OPERATOR_VERSION=12.0.1
 deploy_catalog_source
 
 # IBM MQ
 export OPERATOR_PACKAGE_NAME=ibm-mq
-export OPERATOR_VERSION=3.1.2
+export OPERATOR_VERSION=3.2.0
 deploy_catalog_source
 
 # IBM Event Streams
 export OPERATOR_PACKAGE_NAME=ibm-eventstreams
-export OPERATOR_VERSION=3.3.2
+export OPERATOR_VERSION=3.4.0
 deploy_catalog_source
 
 # IBM Event Endpoint Management (EEM)
 export OPERATOR_PACKAGE_NAME=ibm-eventendpointmanagement
-export OPERATOR_VERSION=11.1.6
+export OPERATOR_VERSION=11.2.0+20240603.010000
 deploy_catalog_source
 
 # IBM DataPower Gateway
 export OPERATOR_PACKAGE_NAME=ibm-datapower-operator
-export OPERATOR_VERSION=1.10.1
+export OPERATOR_VERSION=1.11.0
 deploy_catalog_source
 
 # IBM Aspera HSTS
 export OPERATOR_PACKAGE_NAME=ibm-aspera-hsts-operator
-export OPERATOR_VERSION=1.5.12
+export OPERATOR_VERSION=1.5.13
 deploy_catalog_source
 
 # IBM Cloud Pak foundational services
 export OPERATOR_PACKAGE_NAME=ibm-cp-common-services
-export OPERATOR_VERSION=4.6.1
+export OPERATOR_VERSION=4.6.2
 deploy_catalog_source
 
 # Check that all of the catalog sources are applied correctly
--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # aghowtos
-My personal collection of 'How Tos' - just my 2c - no warranty/support and this is not 'official IBM documentation'
+My personal collection of 'How Tos' - just my 2c - no warranty/support and this is not 'official IBM documentation'.
+
+Use at your own risk, or more practically read them and see if they can save you typing or looking for the relevant documentation which I'll link to.
 
 These are currently in a 'notes I've made' format - if it's useful to you then that's great!
 
