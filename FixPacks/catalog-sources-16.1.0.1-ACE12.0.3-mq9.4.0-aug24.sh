a7395d713ebda3450073ab0877512dd9fb1907d0
andygblue <andyg@uk.ibm.com>
2024-09-04 15:07:05 +0100

    Update to ACE 12.0.3

--- FixPacks/catalog-sources-16.1.0.1-ACE12.0.3-mq9.4.0-aug24.sh
+++ FixPacks/catalog-sources-16.1.0.1-ACE12.0.3-mq9.4.0-aug24.sh
@@ -1,5 +1,5 @@
 # Download and apply catalog sources
-# Updated for CP4I 16.1.0.1 July Fixpack and ACE 12.0.2 Fixpack for SC-2 and MQ 9.4.0
+# Updated for CP4I 16.1.0.1 July Fixpack and ACE 12.0.3 Fixpack for SC-2 and MQ 9.4.0
 # Reference for versions in CP4I Docs here: https://www.ibm.com/docs/en/cloud-paks/cp-integration/16.1.0?topic=images-adding-catalog-sources-cluster
 # Note that you will need to be logged into your cluster through `oc login login <openshift_url> -u <username> -p <password> -n <namespace>` and you will also need to have the ibm-pak in your %PATH%.
 
@@ -24,9 +24,9 @@ export OPERATOR_PACKAGE_NAME=ibm-integration-platform-navigator
 export OPERATOR_VERSION=7.3.1
 deploy_catalog_source
 
-# IBM Automation Assets
+# IBM Automation Assets - update to 1.7.2
 export OPERATOR_PACKAGE_NAME=ibm-integration-asset-repository
-export OPERATOR_VERSION=1.7.0
+export OPERATOR_VERSION=1.7.2
 deploy_catalog_source
 
 # IBM API Connect
@@ -34,9 +34,9 @@ export OPERATOR_PACKAGE_NAME=ibm-apiconnect
 export OPERATOR_VERSION=5.2.0
 deploy_catalog_source
 
-# IBM App Connect 12.0.2 Fixpack
+# IBM App Connect 12.0.3 Fixpack
 export OPERATOR_PACKAGE_NAME=ibm-appconnect
-export OPERATOR_VERSION=12.0.2
+export OPERATOR_VERSION=12.0.3
 deploy_catalog_source
 
 # IBM MQ - update to operator 3.2.3 August MQ 9.4.0
@@ -54,9 +54,9 @@ export OPERATOR_PACKAGE_NAME=ibm-eventendpointmanagement
 export OPERATOR_VERSION=11.2.3
 deploy_catalog_source
 
-# IBM DataPower Gateway
+# IBM DataPower Gateway - Update to 1.11.1
 export OPERATOR_PACKAGE_NAME=ibm-datapower-operator
-export OPERATOR_VERSION=1.11.0
+export OPERATOR_VERSION=1.11.1
 deploy_catalog_source
 
 # IBM Aspera HSTS
@@ -66,7 +66,7 @@ deploy_catalog_source
 
 # IBM Cloud Pak foundational services
 export OPERATOR_PACKAGE_NAME=ibm-cp-common-services
-export OPERATOR_VERSION=4.6.2
+export OPERATOR_VERSION=4.6.5
 deploy_catalog_source
 
 # Check that all of the catalog sources are applied correctly
